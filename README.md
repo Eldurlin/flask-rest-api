59167d3b4d96869a91b2fa8a6474936d7987899f
Eldurlin <adam.cakala@gmail.com>
2020-10-08 11:47:16 +0200

    Initial commit.

---
+++ README.md
@@ -0,0 +1,5 @@
+# Flask-REST-WebApp
+
+## Implementation
+
+This project is implemented using Flask and is a simple REST API for store.
---
+++ app.py
@@ -0,0 +1,26 @@
+from flask import Flask
+from flask_restful import Api
+from flask_jwt import JWT
+from resources.store import Store, StoreList
+from security import authenticate, identity
+from resources.user import UserRegister
+from resources.item import Item, ItemList
+from db import db
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.secret_key = 'ess'
+api = Api(app)
+
+jwt = JWT(app, authenticate, identity)
+
+api.add_resource(Item, '/item/<string:name>')
+api.add_resource(ItemList, '/items')
+api.add_resource(UserRegister, '/register')
+api.add_resource(Store, '/store/<string:name>')
+api.add_resource(StoreList, '/stores')
+
+if __name__ == '__main__':
+    db.init_app(app)
+    app.run(port=5000, debug=True)
