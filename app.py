e89e65f727855220c7a750bacc0a55da3507df86
Eldurlin <adam.cakala@gmail.com>
2020-10-12 11:49:26 +0200

    Added JWT Claims to delete method in item resource.

--- app.py
+++ app.py
@@ -16,6 +16,12 @@ api = Api(app)
 
 jwt = JWTManager(app)
 
+@jwt.user_claims_loader
+def add_claims_to_jwt(identity):
+    if identity == 1:
+        return {'is_admin': True}
+    return {'is_admin': False}
+
 api.add_resource(Item, '/item/<string:name>')
 api.add_resource(ItemList, '/items')
 api.add_resource(UserRegister, '/register')
--- resources/item.py
+++ resources/item.py
@@ -1,5 +1,5 @@
 from flask_restful import Resource, reqparse
-from flask_jwt_extended import jwt_required
+from flask_jwt_extended import jwt_required, get_jwt_claims
 from model.item_model import ItemModel
 
 class Item(Resource):
@@ -30,13 +30,18 @@ class Item(Resource):
         
         return item.json(), 201
 
+    @jwt_required
     def delete(self, name):
+        claims = get_jwt_claims()
+        if not claims['is_admin']:
+            return {'message': 'Admin privilege required.'}, 401
+
         item = ItemModel.find_by_name(name)
 
         if item:
             item.delete_from_db()
-
-        return {'message': 'Item deleted.'}
+            return {'message': 'Item deleted.'}
+        return {'message': 'Item not found.'}, 404
 
     def put(self, name):
         data = Item.parser.parse_args()
